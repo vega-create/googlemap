22355a4b325ce0e12a2b19f7d5c5664e840c4c5c
vega <vegalin1029@gmail.com>
2026-01-30 16:50:13 +0800

    Create layout.tsx

---
+++ layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'GBP 發文工具',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="zh-TW">
+      <body>{children}</body>
+    </html>
+  )
+}
